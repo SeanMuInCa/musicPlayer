dba10baa8e19308c3dbaf9b261ec02a6d6c79189
Sean Mu <fengying1028@gmail.com>
2023-06-02 09:31:38 +0800

    123

--- src/main.js
+++ src/main.js
@@ -7,10 +7,7 @@ import getVant from './plugins/index';
 // 2. 引入组件样式
 import 'vant/lib/index.css'
 // import { Swipe, SwipeItem } from 'vant';
-<<<<<<< HEAD
 import 'vue3-marquee/dist/style.css'
-=======
->>>>>>> 7491713f69f2975d6072ae8a0b62410effde65a7
 let app = createApp(App)
 getVant(app)
 app.use(store).use(router)
